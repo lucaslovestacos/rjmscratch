ee182a93906d132da7e11fc83907a85c7efe5b9c
Lucas Pratt <ljpratt04@gmail.com>
2021-11-04 21:34:51 -0400

    Create scratchClip.js

---
+++ scratchClip.js
@@ -0,0 +1,31 @@
+class ScratchClip {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "Add to Clipboard",
+            "name": "Add to Clipboard",
+            "blocks": [
+                        {
+                            "opcode": "addToClip",
+                            "blockType": "reporter",
+                            "text": "copy [text] to player's clipboard",
+                            "arguments": {
+                                "text": {
+                                    "type": "string",
+                                    "defaultValue": "Hello, Clipboard!"
+                                },
+                            }
+                        }
+                ],
+        };
+    }
+    
+    addToClip({text}) {
+        return navigator.clipboard.writeText(text);
+    }
+    
+}
+
+Scratch.extensions.register(new ScratchClip())
